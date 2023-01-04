61b6f7a1afe1e28bfe0f206e52b83c4e399c2520
Maria Babko <mariababko4292@gmail.com>
2023-01-04 14:29:47 -0800

    Animation assignment files
    Kinda already did everything lol oops

---
+++ main.js
@@ -0,0 +1,17 @@
+const gameEngine = new GameEngine();
+
+const ASSET_MANAGER = new AssetManager();
+
+ASSET_MANAGER.queueDownload("./hollow-knight.png");
+
+ASSET_MANAGER.downloadAll(() => {
+	const canvas = document.getElementById("gameWorld");
+	const ctx = canvas.getContext("2d");
+	ctx.imageSmoothingEnabled = false;
+
+	gameEngine.addEntity(new HollowKnight(gameEngine));
+
+	gameEngine.init(ctx);
+
+	gameEngine.start();
+});
